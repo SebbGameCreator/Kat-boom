dbcc6fb93b87afa598dcda98a6f63f933ea429ba
Ebben <abanalhindi47@gmail.com>
2023-09-18 11:27:51 +0200

    Adjusting movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 25f;
+    public Transform movePoint;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        movePoint.parent = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
 
-        int isDia = x * y != 0 ? 0 : 1;
-        transform.position += new Vector3(x, y, 0) * isDia * Time.deltaTime * speed;
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            if (Mathf.Abs(x) == 1)
+            {
+                movePoint.position += new Vector3(x, 0, 0);
+            }
+            if (Mathf.Abs(y) == 1)
+            {
+                movePoint.position += new Vector3(0, y, 0);
+            }
+        }
+        
+
+        //int isDia = x * y != 0 ? 0 : 1;
+        //transform.position += new Vector3(x, y, 0) * isDia * Time.deltaTime * speed;
     }
 }
